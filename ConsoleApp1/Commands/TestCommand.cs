0d46da68fae3190d7880e143f5b35a4ab7666eed
Cedric Hong <chong12@myseneca.ca>
2021-12-07 20:01:16 -0500

    remove test commands

--- ConsoleApp1/Commands/TestCommand.cs
+++ ConsoleApp1/Commands/TestCommand.cs
@@ -10,20 +10,6 @@ namespace FGGBot.Commands
 {
     public class TestCommand: BaseCommandModule
     {
-        [Command("ping")]
-        [Description("Returns pong")]
-        public async Task Ping(CommandContext ctx) 
-        {
-            await ctx.Channel.SendMessageAsync("Pong").ConfigureAwait(false);
-        }
-
-        [Command("add")]
-        [Description("Adds 2 numbers")]
-        public async Task Add(CommandContext ctx, [Description("First Number")] int numOne, [Description("Second Number")] int numTwo)
-        {
-            await ctx.Channel.SendMessageAsync((numOne + numTwo).ToString()).ConfigureAwait(false);
-        }
-
         [Command("define")]
         [Description("Grabs definition of a word from infil fighting game glossary")]
         public async Task Define(CommandContext ctx, [Description("First Part of Fighting Game Term")] string word1, [Description("First Part of Fighting Game Term")] string word2)
@@ -48,7 +34,5 @@ namespace FGGBot.Commands
             message.WithDescription(definition);
             await ctx.Channel.SendMessageAsync(message).ConfigureAwait(false);
         }
-
-
     }
 }
