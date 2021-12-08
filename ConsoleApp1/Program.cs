5217ca24188b021c2ddc4bd2b88970ce877e3274
Cedric Hong <chong12@myseneca.ca>
2021-12-07 19:51:25 -0500

    Remove comments from main

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,10 +10,6 @@ namespace FGGBot
             var bot = new Bot();
             bot.RunAsync().GetAwaiter().GetResult();
 
-            //var word = new Word("Canadian Burst");
-            //string text = word.GetDefiniton();
-            //Console.WriteLine(text);
-
         }
     }
 }
